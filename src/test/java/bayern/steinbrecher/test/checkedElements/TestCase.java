7ae4f1d01e2d641b4f20643865e1c87f7fb29d0f
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-11-16 21:21:02 +0100

    UT: Add test cases for all creatable JavaFX objects

--- src/test/java/bayern/steinbrecher/test/checkedElements/TestCase.java
+++ src/test/java/bayern/steinbrecher/test/checkedElements/TestCase.java
@@ -1,7 +1,26 @@
 package bayern.steinbrecher.test.checkedElements;
 
+import bayern.steinbrecher.checkedElements.CheckedComboBox;
+import bayern.steinbrecher.checkedElements.CheckedDatePicker;
+import bayern.steinbrecher.checkedElements.CheckedTableView;
 import bayern.steinbrecher.checkedElements.buttons.HelpButton;
+import bayern.steinbrecher.checkedElements.report.ConditionReportTable;
+import bayern.steinbrecher.checkedElements.spinner.CheckedDoubleSpinner;
+import bayern.steinbrecher.checkedElements.spinner.CheckedIntegerSpinner;
+import bayern.steinbrecher.checkedElements.spinner.CheckedSpinner;
+import bayern.steinbrecher.checkedElements.spinner.ContributionField;
+import bayern.steinbrecher.checkedElements.textfields.CharsetTextField;
+import bayern.steinbrecher.checkedElements.textfields.CheckedPasswordField;
+import bayern.steinbrecher.checkedElements.textfields.CheckedRegexTextField;
+import bayern.steinbrecher.checkedElements.textfields.CheckedTextField;
 import bayern.steinbrecher.checkedElements.textfields.NameField;
+import bayern.steinbrecher.checkedElements.textfields.SpecificRegexTextField;
+import bayern.steinbrecher.checkedElements.textfields.sepa.BicTextField;
+import bayern.steinbrecher.checkedElements.textfields.sepa.CheckedSepaTextField;
+import bayern.steinbrecher.checkedElements.textfields.sepa.CreditorIdTextField;
+import bayern.steinbrecher.checkedElements.textfields.sepa.IbanTextField;
+import bayern.steinbrecher.checkedElements.textfields.sepa.MessageIdTextField;
+import bayern.steinbrecher.checkedElements.textfields.sepa.SepaNameField;
 import org.junit.jupiter.params.provider.Arguments;
 
 import java.lang.reflect.InvocationTargetException;
@@ -15,8 +34,33 @@ import java.util.stream.Stream;
 public final class TestCase<T> {
     private static final Logger LOGGER = Logger.getLogger(TestCase.class.getName());
     public static final Collection<TestCase<?>> TEST_CASES = List.of(
+            // bayern.steinbrecher.checkedElements
+            new TestCase<>(CheckedComboBox.class),
+            new TestCase<>(CheckedDatePicker.class),
+            new TestCase<>(CheckedTableView.class),
+            // bayern.steinbrecher.checkedElements.buttons
             new TestCase<>(HelpButton.class),
-            new TestCase<>(NameField.class)
+            // bayern.steinbrecher.checkedElements.report
+            new TestCase<>(ConditionReportTable.class),
+            // bayern.steinbrecher.checkedElements.spinner
+            new TestCase<>(CheckedDoubleSpinner.class),
+            new TestCase<>(CheckedIntegerSpinner.class),
+            new TestCase<>(CheckedSpinner.class),
+            new TestCase<>(ContributionField.class),
+            // bayern.steinbrecher.checkedElements.textfields
+            new TestCase<>(CharsetTextField.class),
+            new TestCase<>(CheckedPasswordField.class),
+            new TestCase<>(CheckedRegexTextField.class),
+            new TestCase<>(CheckedTextField.class),
+            new TestCase<>(NameField.class),
+            new TestCase<>(SpecificRegexTextField.class),
+            // bayern.steinbrecher.checkedElements.textfields
+            new TestCase<>(BicTextField.class),
+            new TestCase<>(CheckedSepaTextField.class),
+            new TestCase<>(CreditorIdTextField.class),
+            new TestCase<>(IbanTextField.class),
+            new TestCase<>(MessageIdTextField.class),
+            new TestCase<>(SepaNameField.class)
     );
 
     private final Class<T> typeOfTestElement;
