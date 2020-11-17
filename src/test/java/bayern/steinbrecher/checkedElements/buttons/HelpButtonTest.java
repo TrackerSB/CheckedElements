ec414d128083cc8403621d1e2ed23a8736de223b
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-11-17 12:45:42 +0100

    HelpButton: Fix order of expected and actual output

--- src/test/java/bayern/steinbrecher/checkedElements/buttons/HelpButtonTest.java
+++ src/test/java/bayern/steinbrecher/checkedElements/buttons/HelpButtonTest.java
@@ -5,7 +5,6 @@ import javafx.event.ActionEvent;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelpButtonTest {
@@ -17,18 +16,18 @@ public class HelpButtonTest {
     @Test
     void verify(){
         final HelpButton defaultButton = new HelpButton();
-        assertEquals(defaultButton.getText(), "?");
-        assertEquals(defaultButton.getHelpMessage(), "");
+        assertEquals("?", defaultButton.getText());
+        assertEquals("", defaultButton.getHelpMessage());
 
         final String label = "Click me";
         final String helpMessageA = "Here could have been help for users";
         final HelpButton customizedButton = new HelpButton(label, helpMessageA);
-        assertEquals(customizedButton.getText(), label);
-        assertEquals(customizedButton.getHelpMessage(), helpMessageA);
+        assertEquals(label, customizedButton.getText());
+        assertEquals(helpMessageA, customizedButton.getHelpMessage());
 
         final String helpMessageB = "This could have been another place for help";
         customizedButton.setHelpMessage(helpMessageB);
-        assertEquals(customizedButton.getHelpMessage(), helpMessageB);
+        assertEquals(helpMessageB, customizedButton.getHelpMessage());
 
         customizedButton.getOnAction()
                 .handle(new ActionEvent());
